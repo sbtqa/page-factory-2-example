2653fe032e6e6b94fbccfe52c40599b8bfd874df
Konstantin Maltsev <mkypers@gmail.com>
2018-07-17 10:14:48 +0700

    add fragments features

--- src/test/java/ru/sbtqa/tag/pagefactory2example/CucumberTest.java
+++ src/test/java/ru/sbtqa/tag/pagefactory2example/CucumberTest.java
@@ -4,13 +4,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true, format = {"pretty"},
-        glue = {"ru.sbtqa.tag.stepdefs",
-            "ru.sbtqa.tag.pagefactory2example.stepdefs",
-            "ru.sbtqa.tag.pagefactory2example.elements.jdi.stepdefs"},
+        glue = {"ru.sbtqa.tag.stepdefs"},
         features = {"src/test/resources/features/"}
+        , tags = {"@fragments"}
 )
 
 public class CucumberTest {}
