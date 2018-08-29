d02b2c25f38f066da1a66e4f9e6c9fc559ae61bb
Konstantin Maltsev <mkypers@gmail.com>
2018-08-29 17:51:01 +0700

    add format

--- src/test/java/ru/sbtqa/tag/pagefactory2example/rest/FillFromFeatureSecondEntry.java
+++ src/test/java/ru/sbtqa/tag/pagefactory2example/rest/FillFromFeatureSecondEntry.java
@@ -1,26 +1,16 @@
 package ru.sbtqa.tag.pagefactory2example.rest;
 
-import cucumber.api.DataTable;
 import ru.sbtqa.tag.api.EndpointEntry;
 import ru.sbtqa.tag.api.Rest;
 import ru.sbtqa.tag.api.annotation.Endpoint;
 import ru.sbtqa.tag.api.annotation.Validation;
 
 import static org.hamcrest.Matchers.equalTo;
-import static ru.sbtqa.tag.api.utils.CastUtils.toMap;
 
-// TODO схлопнуть все get после query applicator
 @Endpoint(method = Rest.GET, path = "search/repositories", title = "найти этот проект на гитхаб через поиск")
 public class FillFromFeatureSecondEntry extends EndpointEntry {
 
-    @Validation(title = "validate")
-    public void validate(DataTable dataTable) {
-        String description = toMap(dataTable).get("description");
-
-        getResponse().body("description", equalTo(description));
-    }
-
-    @Validation(title = "проект")
+    @Validation(title = "количество полученных проектов")
     public void validate() {
         getResponse().body("total_count", equalTo(2));
     }
