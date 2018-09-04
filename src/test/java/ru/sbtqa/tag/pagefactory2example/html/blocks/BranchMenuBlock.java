bff12bc122110d3953b493511af11c7230d9129e
Konstantin Maltsev <mkypers@gmail.com>
2018-09-04 10:14:34 +0700

    remove html name

--- src/test/java/ru/sbtqa/tag/pagefactory2example/html/blocks/BranchMenuBlock.java
+++ src/test/java/ru/sbtqa/tag/pagefactory2example/html/blocks/BranchMenuBlock.java
@@ -2,11 +2,9 @@ package ru.sbtqa.tag.pagefactory2example.html.blocks;
 
 import org.openqa.selenium.support.FindBy;
 import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
-import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.Button;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
-@Name("Список веток")
 @FindBy(xpath = "//div[contains(@class, 'branch-select-menu')]")
 public class BranchMenuBlock extends HtmlElement {
 
