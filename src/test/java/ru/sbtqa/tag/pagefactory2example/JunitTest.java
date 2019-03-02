d90780a9b701c396b57313cf32ea99e20798d4dd
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2019-03-02 16:14:58 +0700

    Example junit configuration

--- src/test/java/ru/sbtqa/tag/pagefactory2example/JunitTest.java
+++ src/test/java/ru/sbtqa/tag/pagefactory2example/JunitTest.java
@@ -3,7 +3,10 @@ package ru.sbtqa.tag.pagefactory2example;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import io.qameta.allure.Step;
 import ru.sbtqa.tag.pagefactory.exceptions.PageException;
+import ru.sbtqa.tag.pagefactory.exceptions.PageInitializationException;
 import ru.sbtqa.tag.stepdefs.CoreSetupSteps;
 import ru.sbtqa.tag.stepdefs.WebSteps;
 
@@ -18,14 +21,34 @@ public class JunitTest {
 
     @Test
     public void webTestTitles() throws PageException, NoSuchMethodException {
-        webSteps.openPage("Test Automation Gears")
-                .fill("Search repositories...", "page-factory-2-example")
-                .click("page-factory-2-example")
-                .openPage("Page-factory-2 example")
-                .action("выбирает ветку", "for-example")
-                .click("example.txt")
-                .openPage("Example")
-                .checkValueIsEqual("Text", "Тестовый текст для примера");
+        openRepository("page-factory-2-example");
+        webSteps.openPage("Page-factory-2 example");
+        openBranch("for-example");
+        checkFile("example.txt", "Тестовый текст для примера");
+      
+
+    }
+
+    @Step("Open repository {name}")
+    private void openRepository(String name) throws PageInitializationException, PageException {
+        webSteps
+            .openPage("Test Automation Gears")
+            .fill("Search repositories...", name)
+            .click(name);
+    }
+
+    @Step("Open branch {name}")
+    private void openBranch(String name) throws PageInitializationException, NoSuchMethodException, PageException {
+        webSteps
+            .action("выбирает ветку", name);
+    }
+
+    @Step("Checks example file {name} with text {text}")
+    private void checkFile(String name, String text) throws PageInitializationException, PageException {
+        webSteps
+            .click("example.txt")
+            .openPage("Example")
+            .checkValueIsEqual("Text", "Тестовый текст для примера");
     }
 
     @AfterClass
