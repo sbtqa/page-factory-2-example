0d53a4dbf11cddc62ecff81c5d7b116b3e5c1ac2
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2022-08-24 17:29:57 +0600

    Try to determine parallel run

--- src/test/java/ru/sbtqa/tag/pagefactory2example/CucumberTest.java
+++ src/test/java/ru/sbtqa/tag/pagefactory2example/CucumberTest.java
@@ -9,6 +9,8 @@ import ru.sbtqa.tag.pagefactory.Tag;
 @CucumberOptions(monochrome = true, plugin = {"pretty"},
         glue = {"ru.sbtqa.tag.stepdefs",
                 "ru.sbtqa.tag.pagefactory2example.stepdefs"},
-        features = {"src/test/resources/en/features/"}
+        features = {"src/test/resources/en/features/"},
+        tags = {"@do_not_run_it"}
+
 )
 public class CucumberTest extends Tag {}
--- src/test/java/ru/sbtqa/tag/pagefactory2example/JunitTest.java
+++ src/test/java/ru/sbtqa/tag/pagefactory2example/JunitTest.java
@@ -1,7 +1,7 @@
 package ru.sbtqa.tag.pagefactory2example;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.sbtqa.tag.pagefactory.exceptions.PageException;
 import ru.sbtqa.tag.pagefactory.junit.CoreSetupSteps;
@@ -13,26 +13,61 @@ import ru.sbtqa.tag.pagefactory2example.web.pages.projects.Pf2ExamplePage;
 
 public class JunitTest {
 
-    private static WebSteps steps;
+    @Before
+    public void before() {
+        CoreSetupSteps.preSetUp();
+        CoreSetupSteps.setUp();
+    }
 
-    @BeforeClass
-    public static void before() {
-        steps = WebSteps.getInstance();
+    private WebSteps steps() {
+        return WebSteps.getInstance();
     }
 
     @Test
     public void webTestTitles() throws PageException {
-        steps.openPage(MainPage.class)
+        steps().openPage(MainPage.class)
                 .selectRepository("page-factory-2-example");
-        steps.openPage(Pf2ExamplePage.class)
+        steps().openPage(Pf2ExamplePage.class)
                 .selectBranch("for-example")
                 .openFile("example.txt");
-        steps.openPage(FilePage.class)
+        steps().openPage(FilePage.class)
+                .validateText("Тестовый текст для примера");
+    }
+    @Test
+    public void webTestTitles2() throws PageException {
+        steps().openPage(MainPage.class)
+                .selectRepository("page-factory-2-example");
+        steps().openPage(Pf2ExamplePage.class)
+                .selectBranch("for-example")
+                .openFile("example.txt");
+        steps().openPage(FilePage.class)
                 .validateText("Тестовый текст для примера");
     }
 
-    @AfterClass
-    public static void after() {
+    @Test
+    public void webTestTitles3() throws PageException {
+        steps().openPage(MainPage.class)
+                .selectRepository("page-factory-2-example");
+        steps().openPage(Pf2ExamplePage.class)
+                .selectBranch("for-example")
+                .openFile("example.txt");
+        steps().openPage(FilePage.class)
+                .validateText("Тестовый текст для примера");
+    }
+
+    @Test
+    public void webTestTitles4() throws PageException {
+        steps().openPage(MainPage.class)
+                .selectRepository("page-factory-2-example");
+        steps().openPage(Pf2ExamplePage.class)
+                .selectBranch("for-example")
+                .openFile("example.txt");
+        steps().openPage(FilePage.class)
+                .validateText("Тестовый текст для примера");
+    }
+
+    @After
+    public void after() {
         CoreSetupSteps.tearDown();
     }
 }
