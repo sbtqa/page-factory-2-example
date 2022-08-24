8ff172ee507c43df521d3c5fad32d55840eeebb9
Krasilnikov-Ilya <fatum0889@mail.ru>
2022-08-24 13:08:36 +0400

    Revert "versions update & working mvn clean test"
    This reverts commit f16fdf6009ae22f55f622d20965df48cb742595c.

--- src/test/java/ru/sbtqa/tag/pagefactory2example/CucumberTest.java
+++ src/test/java/ru/sbtqa/tag/pagefactory2example/CucumberTest.java
@@ -9,6 +9,6 @@ import ru.sbtqa.tag.pagefactory.Tag;
 @CucumberOptions(monochrome = true, plugin = {"pretty"},
         glue = {"ru.sbtqa.tag.stepdefs",
                 "ru.sbtqa.tag.pagefactory2example.stepdefs"},
-        features = {"src/test/resources/ru/features/"}
+        features = {"src/test/resources/en/features/"}
 )
 public class CucumberTest extends Tag {}
